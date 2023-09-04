e159a591072085537bca214133796ccb054d5f2d
JvBR <joaovictor.brodr@gmail.com>
2023-09-04 11:38:55 -0300

    App + Styles + Overlay

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import './components/style.css';
+import { AnimeList } from "./components/AnimeList";
+import { AnimeInfo } from "./components/AnimeInfo";
 
 function App() {
+
+  const [search, setSearch] = useState(``)
+
+  const [animeData, setAnimeData] = useState()
+
+  const [animeInfo, setAnimeInfo] = useState()
+
+  const getData = async () => {
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=20`)
+    const resData = await res.json();
+    setAnimeData(resData.data)
+  }
+
+  useEffect(()=> {
+    getData()
+  },[search])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <h1>MinhaListaAnimes</h1>
+        <div className="search-box">
+          <input type="search" placeholder="Busque um anime" 
+          onChange={(e)=>setSearch(e.target.value)}/>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="animeInfo">
+          {animeInfo && <AnimeInfo animeInfo={animeInfo}/>}
+        </div>
+        <div className="anime-row">
+          <h2 className="text-heading">Anime</h2>
+          <div className="row">
+            <AnimeList animelist={animeData} 
+            setAnimeInfo={setAnimeInfo}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
