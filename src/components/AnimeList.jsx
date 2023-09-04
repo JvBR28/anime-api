710fa7b044d1726ee539cd42ad6584a037259c34
JvBR <joaovictor.brodr@gmail.com>
2023-09-04 11:59:16 -0300

    overlay + sinopsis

--- src/components/AnimeList.jsx
+++ src/components/AnimeList.jsx
@@ -12,7 +12,11 @@ export const AnimeList = ({animelist, setAnimeInfo}) => {
                         <div className='anime-info'>
                             <h4>{anime.title}</h4>
                             <div className='overlay'>
-                                
+                                <h4>{anime.title_japanese}</h4>
+                                <h3>SINOPSE</h3>
+                                <div className='synopsis'>
+                                    <p>{anime.synopsis}</p>
+                                </div>
                             </div>
                         </div>
                     </div>
